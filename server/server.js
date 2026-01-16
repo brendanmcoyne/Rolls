269d8a22c1689ec3c13cdf3d3c057943ffac9345
brendanmcoyne <bmcoyne@bu.edu>
2026-01-16 15:53:57 -0500

    Fixed backend

--- server/server.js
+++ server/server.js
@@ -205,4 +205,10 @@ app.post("/api/unclaim", (req, res) => {
     res.json({ ok: true });
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Backend running on port ${PORT}`);
+});
+
 export default app;
