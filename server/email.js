24dad5022489ccc5709e353bcc5ccd61950ee83f
brendanmcoyne <bmcoyne@bu.edu>
2026-07-09 22:31:38 -0400

    Force Gmail SMTP

--- server/email.js
+++ server/email.js
@@ -1,7 +1,14 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 20000,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_APP_PASSWORD,
